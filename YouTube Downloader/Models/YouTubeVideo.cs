5546576e7871e2cac6d8865cbe71934e2ea485fe
Aleksbgbg <kozichka01@abv.bg>
2018-03-28 19:57:28 +0100

    Add 'YouTubeVideo' model to store data about YouTube videos.

---
+++ YouTube Downloader/Models/YouTubeVideo.cs
@@ -0,0 +1,15 @@
+﻿namespace YouTube.Downloader.Models
+{
+    internal class YouTubeVideo
+    {
+        internal YouTubeVideo(string id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        internal string Id { get; }
+
+        public string Title { get; }
+    }
+}

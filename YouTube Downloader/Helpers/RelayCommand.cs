02c35d2fb7650e481215af6982ed5623f2bc5c93
Aleksbgbg <kozichka01@abv.bg>
2018-03-29 22:39:13 +0100

    Add RelayCommand implementation for commanding.

---
+++ YouTube Downloader/Helpers/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿namespace Fidl.Helpers
+{
+    using System;
+    using System.Windows.Input;
+
+    internal class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+
+        private readonly Predicate<T> _canExecute;
+
+        internal RelayCommand(Action<T> execute) : this(execute, _ => true)
+        {
+        }
+
+        internal RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute((T)parameter);
+        }
+    }
+}

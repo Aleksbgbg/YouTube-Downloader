0dacf666932d29cb4f4749099d42ca106c3c24cb
Aleksbgbg <kozichka01@abv.bg>
2018-03-29 07:57:49 +0100

    Add loading image frames, and a BooleanToVisibilityConverter to show these on loading.

---
+++ YouTube Downloader/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace YouTube.Downloader.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    internal class BooleanToVisibilityConverter : IValueConverter
+    {
+        public static BooleanToVisibilityConverter Default { get; } = new BooleanToVisibilityConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Visibility)value == Visibility.Visible;
+        }
+    }
+}

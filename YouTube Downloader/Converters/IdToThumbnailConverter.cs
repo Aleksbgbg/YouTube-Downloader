0743b6b6a8fced35795fd296ce3d869c8247254c
Aleksbgbg <kozichka01@abv.bg>
2018-03-29 20:49:26 +0100

    Add converter to get thumbnail URL from video ID.

---
+++ YouTube Downloader/Converters/IdToThumbnailConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace YouTube.Downloader.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(string), typeof(string))]
+    internal class IdToThumbnailConverter : IValueConverter
+    {
+        public static IdToThumbnailConverter Default { get; } = new IdToThumbnailConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"https://img.youtube.com/vi/{(string)value}/hqdefault.jpg";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack is not supported.");
+        }
+    }
+}

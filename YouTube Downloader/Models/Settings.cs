658df443055d58bee658e3df4e1bb10d6bd89945
Aleksbgbg <kozichka01@abv.bg>
2018-03-30 22:03:33 +0100

    Add attributes to settings for PropertyGrid.

--- YouTube Downloader/Models/Settings.cs
+++ YouTube Downloader/Models/Settings.cs
@@ -1,7 +1,11 @@
 ﻿namespace YouTube.Downloader.Models
 {
+    using System.ComponentModel;
+
     internal class Settings
     {
+        [DisplayName("Download Path")]
+        [Description("Path where downloaded videos are saved.")]
         public string DownloadPath { get; set; }
     }
 }

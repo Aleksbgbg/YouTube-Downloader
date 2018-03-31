4f51d0799f41151b2ca491543139b5ac901a83a5
Aleksbgbg <kozichka01@abv.bg>
2018-03-31 14:59:58 +0100

    Add the ability to save and close settings, with action buttons (OK / Apply).

--- YouTube Downloader/ViewModels/Interfaces/ISettingsViewModel.cs
+++ YouTube Downloader/ViewModels/Interfaces/ISettingsViewModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace YouTube.Downloader.ViewModels.Interfaces
 {
+    using System;
+
     using YouTube.Downloader.Models;
 
     internal interface ISettingsViewModel : IViewModelBase
     {
         Settings Settings { get; }
+
+        event EventHandler Closed;
     }
 }
--- YouTube Downloader/ViewModels/SettingsViewModel.cs
+++ YouTube Downloader/ViewModels/SettingsViewModel.cs
@@ -1,16 +1,61 @@
 ﻿namespace YouTube.Downloader.ViewModels
 {
+    using System;
+    using System.Collections.Generic;
+
     using YouTube.Downloader.Models;
     using YouTube.Downloader.Services.Interfaces;
     using YouTube.Downloader.ViewModels.Interfaces;
 
     internal class SettingsViewModel : ViewModelBase, ISettingsViewModel
     {
+        private readonly HashSet<string> _changedValues = new HashSet<string>();
+
+        private readonly ISettingsService _settingsService;
+
         public SettingsViewModel(ISettingsService settingsService)
         {
+            _settingsService = settingsService;
+
             Settings = settingsService.Settings;
+
+            Settings.PropertyChanged += (sender, e) =>
+            {
+                _changedValues.Add(e.PropertyName);
+                NotifyOfPropertyChange(() => CanApply);
+            };
         }
 
         public Settings Settings { get; }
+
+        public event EventHandler Closed;
+
+        public void Ok()
+        {
+            if (CanApply)
+            {
+                Apply();
+            }
+
+            Deactivate();
+        }
+
+        public void Cancel()
+        {
+            Deactivate();
+        }
+
+        public bool CanApply => _changedValues.Count > 0;
+
+        public void Apply()
+        {
+            _settingsService.Save();
+            _changedValues.Clear();
+        }
+
+        private void Deactivate()
+        {
+            Closed?.Invoke(this, EventArgs.Empty);
+        }
     }
 }

367ec5a1d7ab5035590550b025cb582d0dc40704
Aleksbgbg <kozichka01@abv.bg>
2018-03-31 18:44:19 +0100

    Add DownloadType enum to choose audio or audio-video downloda types.

---
+++ YouTube Downloader/Models/DownloadType.cs
@@ -0,0 +1,15 @@
+﻿namespace YouTube.Downloader.Models
+{
+    using System.ComponentModel;
+
+    using YouTube.Downloader.Helpers;
+
+    [TypeConverter(typeof(EnumDescriptionConverter))]
+    internal enum DownloadType
+    {
+        [Description("Audio and Video")]
+        AudioVideo,
+        [Description("Audio Only")]
+        Audio
+    }
+}

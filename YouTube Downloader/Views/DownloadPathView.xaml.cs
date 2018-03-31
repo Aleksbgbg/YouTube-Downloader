b86674f9bb7f8e42a81b20254e96f83b69df866e
Aleksbgbg <kozichka01@abv.bg>
2018-03-31 14:57:55 +0100

    Add DownloadPathView, as an editor for DownloadPath in settings.

---
+++ YouTube Downloader/Views/DownloadPathView.xaml.cs
@@ -0,0 +1,52 @@
+﻿namespace YouTube.Downloader.Views
+{
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Forms;
+
+    using Xceed.Wpf.Toolkit.PropertyGrid;
+    using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
+
+    using Binding = System.Windows.Data.Binding;
+
+    public partial class DownloadPathView : ITypeEditor
+    {
+        private static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value",
+                                                                                               typeof(string),
+                                                                                               typeof(DownloadPathView),
+                                                                                               new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public DownloadPathView()
+        {
+            InitializeComponent();
+        }
+
+        public string Value
+        {
+            get => (string)GetValue(ValueProperty);
+
+            set => SetValue(ValueProperty, value);
+        }
+
+        public FrameworkElement ResolveEditor(PropertyItem propertyItem)
+        {
+            Binding binding = new Binding("Value")
+            {
+                Source = propertyItem,
+                Mode = propertyItem.IsReadOnly ? BindingMode.OneWay : BindingMode.TwoWay
+            };
+            BindingOperations.SetBinding(this, ValueProperty, binding);
+
+            return this;
+        }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog { Description = "Select a folder to serve as the downloads folder." };
+
+            if (folderBrowserDialog.ShowDialog() != DialogResult.OK) return;
+
+            Value = folderBrowserDialog.SelectedPath;
+        }
+    }
+}

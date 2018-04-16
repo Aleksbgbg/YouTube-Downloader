32aae79f9acf27333fe66ced4b40d64a6f706fc2
Aleksbgbg <kozichka01@abv.bg>
2018-04-16 18:21:39 +0100

    Add sorting for downloads depending on DownloadState.

--- YouTube Downloader/ViewModels/CurrentDownloadsViewModel.cs
+++ YouTube Downloader/ViewModels/CurrentDownloadsViewModel.cs
@@ -2,8 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Linq;
     using System.Threading.Tasks;
+    using System.Windows.Data;
 
     using Caliburn.Micro;
 
@@ -22,25 +24,35 @@
             eventAggregator.Subscribe(this);
             _downloadService = downloadService;
             _downloadFactory = downloadFactory;
+
+            ((ListCollectionView)CollectionViewSource.GetDefaultView(Downloads)).CustomSort = Comparer<IDownloadViewModel>.Create((first, second) => -first.DownloadState.CompareTo(second.DownloadState));
         }
 
         public IObservableCollection<IDownloadViewModel> Downloads { get; } = new BindableCollection<IDownloadViewModel>();
 
         public void Handle(IEnumerable<IVideoViewModel> message)
         {
-            IDownloadViewModel[] newDownloads = message.Select(downloadVideo => _downloadFactory.MakeDownloadViewModel(downloadVideo)).ToArray();
+            IDownloadViewModel[] newDownloads = message.Select(_downloadFactory.MakeDownloadViewModel).ToArray();
 
             foreach (IDownloadViewModel download in newDownloads)
             {
                 void DownloadViewModelDownloadCompleted(object sender, EventArgs e)
                 {
                     download.DownloadCompleted -= DownloadViewModelDownloadCompleted;
+                    download.PropertyChanged -= DownloadPropertyChanged;
 
                     Task.Delay(3_000).ContinueWith(task => Downloads.Remove(download));
                 }
 
                 download.DownloadCompleted += DownloadViewModelDownloadCompleted;
             }
+
+            void DownloadPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                Downloads.Refresh();
+            }
+
+            newDownloads.Apply(download => download.PropertyChanged += DownloadPropertyChanged);
 
             Downloads.AddRange(newDownloads);
 

9ee912f060ab8cf458dd3c44b795b847ab9ca67a
Aleksbgbg <kozichka01@abv.bg>
2018-04-17 07:44:54 +0100

    Add a Mutex to App class to make YTD a single-instance application.

--- YouTube Downloader/App.xaml.cs
+++ YouTube Downloader/App.xaml.cs
@@ -1,11 +1,23 @@
 ﻿namespace YouTube.Downloader
 {
+    using System.Threading;
     using System.Windows;
 
     public partial class App
     {
+        private readonly Mutex _mutex;
+
         public App()
         {
+            if (Mutex.TryOpenExisting("YouTube Downloader", out Mutex _))
+            {
+                MessageBox.Show("Another instance of YouTube Downloader is already open.", "Instance Already Open", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Current.Shutdown();
+                return;
+            }
+
+            _mutex = new Mutex(true, "YouTube Downloader");
+
             Dispatcher.UnhandledException += (sender, e) =>
             {
                 e.Handled = true;

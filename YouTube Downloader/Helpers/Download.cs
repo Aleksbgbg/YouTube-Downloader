0363d62a441700a645155e638d7261052f70debd
Aleksbgbg <kozichka01@abv.bg>
2018-04-25 18:41:40 +0100

    Add 'Download' class to manage video downloads.

---
+++ YouTube Downloader/Helpers/Download.cs
@@ -0,0 +1,79 @@
+﻿namespace YouTube.Downloader.Helpers
+{
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    using YouTube.Downloader.Models;
+
+    internal class Download
+    {
+        private readonly List<string> _processArguments;
+
+        internal Download(YouTubeVideo video, Settings settings)
+        {
+            _processArguments = new List<string>
+            {
+                $"-o \"{settings.DownloadPath}/%(title)s.%(ext)s",
+                $"-f {(settings.DownloadType == DownloadType.Audio ? "bestaudio" : "bestvideo+bestaudio")}",
+                $"\"{video.Id}\""
+            };
+        }
+
+        private Process _process;
+        public Process Process
+        {
+            get
+            {
+                if (_process == null)
+                {
+                    GenerateProcess();
+                }
+
+                return _process;
+            }
+
+            private set => _process = value;
+        }
+
+        internal void Start()
+        {
+            if (_processArguments.Contains("-c"))
+            {
+                _processArguments.Remove("-c");
+            }
+
+            Process.Start();
+        }
+
+        internal void Pause()
+        {
+            Process = null;
+        }
+
+        internal void Resume()
+        {
+            _processArguments.Add("-c");
+            GenerateProcess();
+            Process.Start();
+        }
+
+        internal void Kill()
+        {
+            Process.Kill();
+        }
+
+        private void GenerateProcess()
+        {
+            Process = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo = new ProcessStartInfo("Resources/youtube-dl.exe", string.Join(" ", _processArguments))
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                }
+            };
+        }
+    }
+}

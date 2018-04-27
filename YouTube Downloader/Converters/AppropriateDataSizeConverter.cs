991b0c26a4d8e8fb31da7dd6fb1d83f9243ea2ad
Aleksbgbg <kozichka01@abv.bg>
2018-04-27 21:48:21 +0100

    Add AppropriateDataSizeConverter to convert from bytes to correct data size.

---
+++ YouTube Downloader/Converters/AppropriateDataSizeConverter.cs
@@ -0,0 +1,34 @@
+﻿namespace YouTube.Downloader.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(long), typeof(string))]
+    internal class AppropriateDataSizeConverter : IValueConverter
+    {
+        private static readonly char[] StoragePrefixes = { 'K', 'M', 'G', 'T', 'P', 'E' };
+
+        public static AppropriateDataSizeConverter Default { get; } = new AppropriateDataSizeConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double bytes = (long)value;
+
+            int loopCount = 0;
+
+            while (bytes >= 1024)
+            {
+                bytes /= 1024;
+                ++loopCount;
+            }
+
+            return loopCount == 0 ? $"{bytes:N0} B" : $"{bytes:N2} {StoragePrefixes[loopCount - 1]}iB";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack is not supported.");
+        }
+    }
+}

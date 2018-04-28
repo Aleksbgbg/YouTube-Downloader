d0d70e69866414171cdeb88477ad8c6ff0de8294
Aleksbgbg <kozichka01@abv.bg>
2018-04-28 21:22:50 +0100

    Convert CurrentDownloadsView to use ListBox instead of ItemsControl, with a CheckBox on each item.

---
+++ YouTube Downloader/Converters/BooleanToHiddenVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace YouTube.Downloader.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    internal class BooleanToHiddenVisibilityConverter : IValueConverter
+    {
+        public static BooleanToHiddenVisibilityConverter Default { get; } = new BooleanToHiddenVisibilityConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack is not supported.");
+        }
+    }
+}

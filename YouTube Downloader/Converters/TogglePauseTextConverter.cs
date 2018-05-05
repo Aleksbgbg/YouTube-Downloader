974a0e4072a2ddf7894a6d373172b50215aa7c39
Aleksbgbg <kozichka01@abv.bg>
2018-05-05 21:53:45 +0100

    [TogglePauseTextConverter] Add converter to change the button text for TogglePause action.

---
+++ YouTube Downloader/Converters/TogglePauseTextConverter.cs
@@ -0,0 +1,36 @@
+﻿namespace YouTube.Downloader.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using YouTube.Downloader.Core;
+
+    [ValueConversion(typeof(bool), typeof(string))]
+    internal class TogglePauseTextConverter : IMultiValueConverter
+    {
+        public static TogglePauseTextConverter Default { get; } = new TogglePauseTextConverter();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool canPause = (bool)values[0];
+            bool canResume = (bool)values[1];
+
+            if (canPause)
+            {
+                return "Pause";
+            }
+
+            if (canResume)
+            {
+                return "Resume";
+            }
+
+            return "Toggle Pause";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw ThrowHelper.NotSupported();
+        }
+    }
+}

b77f96624751e84fbfc5bd0d58dd462f5673cfaa
Aleksbgbg <kozichka01@abv.bg>
2018-05-05 13:14:48 +0100

    Add DownloadStatus class, to encapsulate a DownloadProgress and a DownloadState.

---
+++ YouTube Downloader/Models/Download/DownloadStatus.cs
@@ -0,0 +1,35 @@
+﻿namespace YouTube.Downloader.Models.Download
+{
+    using Caliburn.Micro;
+
+    internal class DownloadStatus : PropertyChangedBase
+    {
+        private DownloadState _downloadState = DownloadState.Queued;
+        public DownloadState DownloadState
+        {
+            get => _downloadState;
+
+            set
+            {
+                if (_downloadState == value) return;
+
+                _downloadState = value;
+                NotifyOfPropertyChange(() => DownloadState);
+            }
+        }
+
+        private DownloadProgress _downloadProgress = new DownloadProgress();
+        public DownloadProgress DownloadProgress
+        {
+            get => _downloadProgress;
+
+            set
+            {
+                if (_downloadProgress == value) return;
+
+                _downloadProgress = value;
+                NotifyOfPropertyChange(() => DownloadProgress);
+            }
+        }
+    }
+}

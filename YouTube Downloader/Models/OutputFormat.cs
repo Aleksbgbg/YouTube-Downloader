69946db9e5e65a864f837b9ce91298ec8a14e6f7
Aleksbgbg <kozichka01@abv.bg>
2018-05-14 19:40:32 +0100

    [OutputFormat] Add OutputFormat enum to allow users to choose MP3, MP4, or Auto download types.

---
+++ YouTube Downloader/Models/OutputFormat.cs
@@ -0,0 +1,16 @@
+﻿namespace YouTube.Downloader.Models
+{
+    using System.ComponentModel;
+
+    using YouTube.Downloader.Core;
+
+    [TypeConverter(typeof(EnumDescriptionConverter))]
+    internal enum OutputFormat
+    {
+        [Description("MP3")]
+        Mp3,
+        [Description("MP4")]
+        Mp4,
+        Auto
+    }
+}

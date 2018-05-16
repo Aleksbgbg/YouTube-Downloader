db215183e65ba58c89b17cf37bec387b59adad69
Aleksbgbg <kozichka01@abv.bg>
2018-05-16 20:55:17 +0100

    [Download] Add support for converting videos. Move 'Settings' parameter to static readonly variable.

--- YouTube Downloader/Core/Downloading/Download.cs
+++ YouTube Downloader/Core/Downloading/Download.cs
@@ -1,29 +1,36 @@
 ﻿namespace YouTube.Downloader.Core.Downloading
 {
     using System;
-    using System.Diagnostics;
     using System.IO;
+    using System.Text.RegularExpressions;
+
+    using Caliburn.Micro;
 
     using YouTube.Downloader.EventArgs;
     using YouTube.Downloader.Models;
     using YouTube.Downloader.Models.Download;
+    using YouTube.Downloader.Services.Interfaces;
 
     internal class Download
     {
+        private static readonly Settings Settings = IoC.Get<ISettingsService>().Settings;
+
         private readonly string _processArguments;
 
         private readonly DownloadStatus _downloadStatus;
 
         private bool _isRequeued;
 
-        internal Download(DownloadStatus downloadStatus, Settings settings, YouTubeVideo youTubeVideo)
+        private MonitoredProcess _monitoredProcess;
+
+        internal Download(DownloadStatus downloadStatus, YouTubeVideo youTubeVideo)
         {
             string GetFormat()
             {
-                switch (settings.OutputFormat)
+                switch (Settings.OutputFormat)
                 {
                     case OutputFormat.Auto:
-                        switch (settings.DownloadType)
+                        switch (Settings.DownloadType)
                         {
                             case DownloadType.AudioVideo:
                                 return "bestvideo+bestaudio";
@@ -32,7 +39,7 @@
                                 return "bestaudio";
 
                             default:
-                                throw new InvalidOperationException("Download started with invalid settings.");
+                                throw new InvalidOperationException("Download started with invalid Settings.");
                         }
 
                     case OutputFormat.Mp4:
@@ -42,37 +49,13 @@
                         return "mp3/bestaudio";
 
                     default:
-                        throw new InvalidOperationException("Download started with invalid settings.");
+                        throw new InvalidOperationException("Download started with invalid Settings.");
                 }
             }
 
             _downloadStatus = downloadStatus;
-            _processArguments = $"-o \"{settings.DownloadPath}\\%(title)s.%(ext)s\" -f {GetFormat()} -- \"{youTubeVideo.Id}\"";
+            _processArguments = $"-o \"{Settings.DownloadPath}\\%(title)s.%(ext)s\" -f {GetFormat()} -- \"{youTubeVideo.Id}\"";
             YouTubeVideo = youTubeVideo;
-
-            Finished += (sender, e) =>
-            {
-                if (!e.DidComplete || settings.OutputFormat == OutputFormat.Auto)
-                {
-                    return;
-                }
-
-                string destination = (string)DownloadProcess.ProcessMonitor.ParameterMonitorings["Destination"].Value;
-
-                if (destination == null)
-                {
-                    return;
-                }
-
-                FileInfo destinationInfo = new FileInfo(destination);
-
-                string expectedExtension = settings.OutputFormat == OutputFormat.Mp4 ? ".mp4" : ".mp3";
-
-                if (destinationInfo.Extension != expectedExtension)
-                {
-                    Process.Start("Resources\\ffmpeg.exe", $"-i \"{destinationInfo.FullName}\" \"{Path.ChangeExtension(destinationInfo.FullName, expectedExtension)}\"");
-                }
-            };
         }
 
         internal event EventHandler<DownloadFinishedEventArgs> Finished;
@@ -139,68 +122,6 @@
             {
                 _didComplete = value;
                 UpdateDownloadState();
-            }
-        }
-
-        private DownloadProcess _downloadProcess;
-        private DownloadProcess DownloadProcess
-        {
-            get => _downloadProcess;
-
-            set
-            {
-                void DownloadProcessExited(object sender, EventArgs e)
-                {
-                    _downloadProcess.Exited -= DownloadProcessExited;
-
-                    if (IsPaused || HasExited)
-                    {
-                        return;
-                    }
-
-                    DidComplete = true;
-                    HasExited = true;
-                }
-
-                if (_downloadProcess != null)
-                {
-                    _downloadProcess.Exited -= DownloadProcessExited;
-                }
-
-                _downloadProcess = value;
-
-                if (_downloadProcess == null)
-                {
-                    return;
-                }
-
-                _downloadProcess.Exited += DownloadProcessExited;
-
-                ParameterMonitoring progressMonitoring = _downloadProcess.ProcessMonitor.ParameterMonitorings["Progress"];
-
-                _downloadProcess.ProcessMonitor.Finished += ProcessMonitorFinished;
-                progressMonitoring.ValueUpdated += ProgressUpdated;
-
-                void ProcessMonitorFinished(object sender, EventArgs e)
-                {
-                    _downloadProcess.ProcessMonitor.Finished -= ProcessMonitorFinished;
-                    progressMonitoring.ValueUpdated -= ProgressUpdated;
-                }
-
-                void ProgressUpdated(object sender, ParameterUpdatedEventArgs e)
-                {
-                    Progress progress = (Progress)e.NewValue;
-
-                    _downloadStatus.DownloadProgress.Stage = progress.Stage;
-
-                    if (progress.DownloadSpeed.HasValue)
-                    {
-                        _downloadStatus.DownloadProgress.DownloadSpeed = progress.DownloadSpeed.Value;
-                    }
-
-                    _downloadStatus.DownloadProgress.ProgressPercentage = progress.DownloadPercentage;
-                    _downloadStatus.DownloadProgress.TotalDownloadSize = progress.TotalDownloadSize;
-                }
             }
         }
 
@@ -275,14 +196,153 @@
 
         private void GenerateAndStartProcess()
         {
-            DownloadProcess = new DownloadProcess(_processArguments);
-            DownloadProcess.Start();
+            _monitoredProcess = new MonitoredProcess("youtube-dl", _processArguments);
+
+            void DownloadProcessExited(object sender, EventArgs e)
+            {
+                _monitoredProcess.Exited -= DownloadProcessExited;
+
+                if (IsPaused || HasExited)
+                {
+                    return;
+                }
+
+                if (Settings.OutputFormat != OutputFormat.Auto)
+                {
+                    string destination = (string)_monitoredProcess.ProcessMonitor.ParameterMonitorings["Destination"].Value;
+
+                    if (destination == null)
+                    {
+                        return;
+                    }
+
+                    FileInfo destinationInfo = new FileInfo(destination);
+
+                    string expectedExtension = Settings.OutputFormat == OutputFormat.Mp4 ? ".mp4" : ".mp3";
+
+                    if (destinationInfo.Extension != expectedExtension)
+                    {
+                        _monitoredProcess = new MonitoredProcess("ffmpeg", $"-i \"{destinationInfo.FullName}\" \"{Path.ChangeExtension(destinationInfo.FullName, expectedExtension)}\"");
+
+                        void ConversionProcessExited(object _, EventArgs args)
+                        {
+                            _monitoredProcess.Exited -= ConversionProcessExited;
+
+                            DidComplete = true;
+                            HasExited = true;
+                        }
+
+                        _monitoredProcess.Exited += ConversionProcessExited;
+                        _monitoredProcess.Start();
+
+                        _downloadStatus.DownloadState = DownloadState.Converting;
+
+                        return;
+                    }
+                }
+
+                DidComplete = true;
+                HasExited = true;
+            }
+
+            _monitoredProcess.Exited += DownloadProcessExited;
+
+            {
+                int stage = 0;
+                double lastProgress = 0;
+
+                foreach (ParameterMonitoring parameterMonitoring in new ParameterMonitoring[]
+                {
+                        new ParameterMonitoring("Progress", new Regex(@"^\[download] (?<ProgressPercentage>[ 1][ 0-9][0-9]\.[0-9])% of .*?(?<TotalDownloadSize>[\d\.]+)?(?<TotalDownloadSizeUnits>.iB) at +(?:(?<DownloadSpeed>.+)(?<DownloadSpeedUnits>.iB)\/s|Unknown speed)"), match =>
+                        {
+                            long GetBytes(double size, string units)
+                            {
+                                int GetMultiplier()
+                                {
+                                    switch (units)
+                                    {
+                                        case "KiB":
+                                            return 1024;
+
+                                        case "MiB":
+                                            return 1024 * 1024;
+
+                                        case "GiB":
+                                            return 1024 * 1024 * 1024;
+
+                                        default:
+                                            throw new InvalidOperationException("Invalid units for multiplier.");
+                                    }
+                                }
+
+                                return (long)(size * GetMultiplier());
+                            }
+
+                            long? GetDownloadSpeed()
+                            {
+                                string downloadSpeed = match.Groups["DownloadSpeed"].Value;
+
+                                if (downloadSpeed == string.Empty)
+                                {
+                                    return null;
+                                }
+
+                                return GetBytes(double.Parse(downloadSpeed), match.Groups["DownloadSpeedUnits"].Value);
+                            }
+
+                            long totalDownloadSize = GetBytes(double.Parse(match.Groups["TotalDownloadSize"].Value), match.Groups["TotalDownloadSizeUnits"].Value);
+
+                            double progressPercentage = double.Parse(match.Groups["ProgressPercentage"].Value);
+
+                            if (stage == 0 || progressPercentage >= 100 && lastProgress < 100)
+                            {
+                                ++stage;
+                            }
+
+                            lastProgress = progressPercentage;
+
+                            return new Progress(totalDownloadSize, progressPercentage, GetDownloadSpeed(), stage);
+                        }),
+                        new ParameterMonitoring("Destination", new Regex(@"^\[download] Destination: (?<Filename>.+)$"), match => match.Groups["Filename"].Value)
+                })
+                {
+                    _monitoredProcess.ProcessMonitor.AddParameterMonitoring(parameterMonitoring);
+                }
+            }
+
+            ParameterMonitoring progressMonitoring = _monitoredProcess.ProcessMonitor.ParameterMonitorings["Progress"];
+
+            _monitoredProcess.ProcessMonitor.Finished += ProcessMonitorFinished;
+            progressMonitoring.ValueUpdated += ProgressUpdated;
+
+            void ProcessMonitorFinished(object sender, EventArgs e)
+            {
+                _monitoredProcess.ProcessMonitor.Finished -= ProcessMonitorFinished;
+                progressMonitoring.ValueUpdated -= ProgressUpdated;
+            }
+
+            void ProgressUpdated(object sender, ParameterUpdatedEventArgs e)
+            {
+                Progress progress = (Progress)e.NewValue;
+
+                _downloadStatus.DownloadProgress.Stage = progress.Stage;
+
+                if (progress.DownloadSpeed.HasValue)
+                {
+                    _downloadStatus.DownloadProgress.DownloadSpeed = progress.DownloadSpeed.Value;
+                }
+
+                _downloadStatus.DownloadProgress.ProgressPercentage = progress.DownloadPercentage;
+                _downloadStatus.DownloadProgress.TotalDownloadSize = progress.TotalDownloadSize;
+            }
+
+            _monitoredProcess.Start();
         }
 
         private void KillProcess()
         {
-            DownloadProcess.Kill();
-            DownloadProcess = null;
+            _monitoredProcess.Kill();
+            _monitoredProcess = null;
         }
 
         private void UpdateDownloadState()

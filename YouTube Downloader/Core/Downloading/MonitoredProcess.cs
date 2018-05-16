c5c360202c4e92ed9e941a53ca2edf363593a5ce
Aleksbgbg <kozichka01@abv.bg>
2018-05-16 20:53:05 +0100

    [MonitoredProcess] Rename 'DownloadProcess' to 'MonitoredProcess', and remove ParameterMonitoring initialisation logic to make this a uniform process monitor.

---
+++ YouTube Downloader/Core/Downloading/MonitoredProcess.cs
@@ -0,0 +1,46 @@
+﻿namespace YouTube.Downloader.Core.Downloading
+{
+    using System;
+    using System.Diagnostics;
+
+    internal class MonitoredProcess
+    {
+        internal MonitoredProcess(string process, string arguments)
+        {
+            Process = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo = new ProcessStartInfo($"Resources/{process}.exe", arguments)
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                }
+            };
+
+            ProcessMonitor = new ProcessMonitor(Process);
+        }
+
+        internal event EventHandler Exited
+        {
+            add => Process.Exited += value;
+
+            remove => Process.Exited -= value;
+        }
+
+        internal Process Process { get; }
+
+        internal ProcessMonitor ProcessMonitor { get; }
+
+        internal void Start()
+        {
+            Process.Start();
+            ProcessMonitor.Run();
+        }
+
+        internal void Kill()
+        {
+            Process.Kill();
+        }
+    }
+}

f6a91840208b2e44a45a4d89d0f7dc587747e1c0
Aleksbgbg <kozichka01@abv.bg>
2018-05-16 20:06:38 +0100

    [ProcessMonitor] Rename 'ProgressMonitor' to 'ProcessMonitor', accepting several parameters indicating how output should be handled.

---
+++ YouTube Downloader/Core/Downloading/ParameterMonitoring.cs
@@ -0,0 +1,42 @@
+﻿namespace YouTube.Downloader.Core.Downloading
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    using YouTube.Downloader.EventArgs;
+
+    internal class ParameterMonitoring
+    {
+        private readonly Func<Match, object> _matchProcessor;
+
+        internal ParameterMonitoring(string name, Regex regex, Func<Match, object> matchProcessor)
+        {
+            Name = name;
+            Regex = regex;
+            _matchProcessor = matchProcessor;
+        }
+
+        internal event EventHandler<ParameterUpdatedEventArgs> ValueUpdated;
+
+        internal string Name { get; }
+
+        internal Regex Regex { get; }
+
+        private object _value;
+        internal object Value
+        {
+            get => _value;
+
+            private set
+            {
+                _value = value;
+                ValueUpdated?.Invoke(this, new ParameterUpdatedEventArgs(_value));
+            }
+        }
+
+        internal void Update(Match match)
+        {
+            Value = _matchProcessor(match);
+        }
+    }
+}
---
+++ YouTube Downloader/Core/Downloading/ProcessMonitor.cs
@@ -0,0 +1,66 @@
+﻿namespace YouTube.Downloader.Core.Downloading
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    internal class ProcessMonitor
+    {
+        private readonly Process _process;
+
+        internal ProcessMonitor(Process process, IEnumerable<ParameterMonitoring> parameterMonitorings)
+        {
+            _process = process;
+
+            foreach (ParameterMonitoring parameterMonitoring in parameterMonitorings)
+            {
+                ParameterMonitorings[parameterMonitoring.Name] = parameterMonitoring;
+            }
+        }
+
+        internal event EventHandler Finished;
+
+        internal Dictionary<string, ParameterMonitoring> ParameterMonitorings { get; } = new Dictionary<string, ParameterMonitoring>();
+
+        internal void Run()
+        {
+            RunMonitoringThread();
+        }
+
+        private void RunMonitoringThread()
+        {
+            Task.Run(() =>
+            {
+                using (StreamReader progressReader = _process.StandardOutput)
+                {
+                    try
+                    {
+                        while (!progressReader.EndOfStream)
+                        {
+                            string line = progressReader.ReadLine();
+
+                            if (line == null) continue;
+
+                            foreach (ParameterMonitoring parameterMonitoring in ParameterMonitorings.Values)
+                            {
+                                Match regexMatch = parameterMonitoring.Regex.Match(line);
+
+                                if (regexMatch.Success)
+                                {
+                                    parameterMonitoring.Update(regexMatch);
+                                }
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        Finished?.Invoke(this, EventArgs.Empty);
+                    }
+                }
+            });
+        }
+    }
+}

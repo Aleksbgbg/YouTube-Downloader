bab7b2cf8c128072730c11265f62e95ede0729b4
Aleksbgbg <kozichka01@abv.bg>
2018-05-18 22:53:15 +0100

    [MatchedVideoViewModel] Add buttons to exchange video for another match using RequeryView, which show on hover over matched video.

--- YouTube Downloader/ViewModels/MatchedVideoViewModel.cs
+++ YouTube Downloader/ViewModels/MatchedVideoViewModel.cs
@@ -1,14 +1,31 @@
 ﻿namespace YouTube.Downloader.ViewModels
 {
+    using YouTube.Downloader.Factories.Interfaces;
+    using YouTube.Downloader.Services.Interfaces;
     using YouTube.Downloader.ViewModels.Interfaces;
 
     internal class MatchedVideoViewModel : ViewModelBase, IMatchedVideoViewModel
     {
+        private readonly IRequeryFactory _requeryFactory;
+
+        private readonly IDialogService _dialogService;
+
+        public MatchedVideoViewModel(IRequeryFactory requeryFactory, IDialogService dialogService)
+        {
+            _requeryFactory = requeryFactory;
+            _dialogService = dialogService;
+        }
+
         public IVideoViewModel VideoViewModel { get; private set; }
 
         public void Initialise(IVideoViewModel videoViewModel)
         {
             VideoViewModel = videoViewModel;
+        }
+
+        public void Exchange()
+        {
+            _dialogService.ShowDialog(_requeryFactory.MakeRequeryViewModel(VideoViewModel));
         }
     }
 }

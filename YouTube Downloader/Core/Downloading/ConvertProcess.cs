3e5b269b8f71a7e0b8c75789de8d23fd42b478f3
Aleksbgbg <kozichka01@abv.bg>
2018-05-29 18:56:13 +0100

    [ConvertProcess] Add process class for ffmpeg conversions.

---
+++ YouTube Downloader/Core/Downloading/ConvertProcess.cs
@@ -0,0 +1,11 @@
+﻿namespace YouTube.Downloader.Core.Downloading
+{
+    using System.IO;
+
+    internal class ConvertProcess : MonitoredProcess
+    {
+        internal ConvertProcess(string filename, string newExtension) : base("ffmpeg", $"-i \"{filename}\" \"{Path.ChangeExtension(filename, newExtension)}\"")
+        {
+        }
+    }
+}

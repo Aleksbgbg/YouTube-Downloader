043ee338ef277a5f6ad2dca1253470ae37d4c82a
Aleksbgbg <kozichka01@abv.bg>
2018-05-29 12:54:08 +0100

    [QueryResult] Add QueryResult class to store information about a query, a matched YouTubeVideo based on the query, and the percentage of certainty in the match (between the video title and the actual query).

---
+++ YouTube Downloader/Models/QueryResult.cs
@@ -0,0 +1,43 @@
+﻿namespace YouTube.Downloader.Models
+{
+    using Newtonsoft.Json;
+
+    using YouTube.Downloader.Extensions;
+
+    internal class QueryResult
+    {
+        internal QueryResult(string query)
+        {
+            Query = query;
+        }
+
+        [JsonConstructor]
+        internal QueryResult(string query, YouTubeVideo matchedVideo) : this(query)
+        {
+            MatchedVideo = matchedVideo;
+        }
+
+        internal QueryResult(QueryResult queryResult, YouTubeVideo matchedVideo) : this(queryResult.Query, matchedVideo)
+        {
+        }
+
+        [JsonProperty("Query")]
+        public string Query { get; }
+
+        private YouTubeVideo _matchedVideo;
+        [JsonProperty("MatchedVideo")]
+        public YouTubeVideo MatchedVideo
+        {
+            get => _matchedVideo;
+
+            set
+            {
+                _matchedVideo = value;
+                PercentageMatch = Query.PercentageSimilarity(_matchedVideo.Title);
+            }
+        }
+
+        [JsonIgnore]
+        public double PercentageMatch { get; private set; }
+    }
+}

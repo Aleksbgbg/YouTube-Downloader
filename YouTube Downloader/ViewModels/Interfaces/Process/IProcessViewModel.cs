6554da52cca01fad7351515b37cd64da30fbbde3
Aleksbgbg <kozichka01@abv.bg>
2018-05-30 14:05:37 +0100

    [ProcessViewModel] Add ProcessViewModel to replace DownloadViewModel.

---
+++ YouTube Downloader/ViewModels/Interfaces/Process/IProcessViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace YouTube.Downloader.ViewModels.Interfaces.Process
+{
+    using YouTube.Downloader.Core.Downloading;
+    using YouTube.Downloader.Models.Download;
+
+    internal interface IProcessViewModel : IViewModelBase
+    {
+        IVideoViewModel VideoViewModel { get; }
+
+        MonitoredProcess Process { get; }
+
+        DownloadStatus DownloadStatus { get; }
+
+        void Initialise(IVideoViewModel videoViewModel, MonitoredProcess process);
+    }
+}
---
+++ YouTube Downloader/ViewModels/Process/ProcessViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace YouTube.Downloader.ViewModels.Process
+{
+    using YouTube.Downloader.Core.Downloading;
+    using YouTube.Downloader.Models.Download;
+    using YouTube.Downloader.ViewModels.Interfaces;
+    using YouTube.Downloader.ViewModels.Interfaces.Process;
+
+    internal class ProcessViewModel : ViewModelBase, IProcessViewModel
+    {
+        public IVideoViewModel VideoViewModel { get; private set; }
+
+        public MonitoredProcess Process { get; private set; }
+
+        public DownloadStatus DownloadStatus { get; } = new DownloadStatus();
+
+        public void Initialise(IVideoViewModel videoViewModel, MonitoredProcess process)
+        {
+            VideoViewModel = videoViewModel;
+            Process = process;
+        }
+    }
+}

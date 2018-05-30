d727551ac9ee62fc7d875eb07be017f61a526ca0
Aleksbgbg <kozichka01@abv.bg>
2018-05-30 14:51:21 +0100

    [IConversionService] Add missing 'QueueConversion' method declaration.

--- YouTube Downloader/Services/Interfaces/IConversionService.cs
+++ YouTube Downloader/Services/Interfaces/IConversionService.cs
@@ -1,6 +1,11 @@
 ﻿namespace YouTube.Downloader.Services.Interfaces
 {
+    using System.Collections.Generic;
+
+    using YouTube.Downloader.Core.Downloading;
+
     internal interface IConversionService
     {
+        void QueueConversion(IEnumerable<ConvertProcess> conversions);
     }
 }

d4beec78389b6ad91c97e2959ba465f144f3dba6
Aleksbgbg <kozichka01@abv.bg>
2018-05-30 17:45:23 +0100

    [ResourceConverter] Add converter to retrieve resources from string.

---
+++ YouTube Downloader/Converters/ResourceConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace YouTube.Downloader.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    using YouTube.Downloader.Core;
+
+    [ValueConversion(typeof(string), typeof(object))]
+    internal class ResourceConverter : IValueConverter
+    {
+        public static ResourceConverter Default { get; } = new ResourceConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Application.Current.FindResource((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw ThrowHelper.NotSupported();
+        }
+    }
+}

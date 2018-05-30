31739298b34784bef6e9ac9198db280aefd157c9
Aleksbgbg <kozichka01@abv.bg>
2018-05-30 17:27:32 +0100

    [ConvertProcess] Ensure original file is deleted after completion.

--- YouTube Downloader/Core/Downloading/ConvertProcess.cs
+++ YouTube Downloader/Core/Downloading/ConvertProcess.cs
@@ -6,8 +6,19 @@
 
     internal class ConvertProcess : MonitoredProcess
     {
+        private readonly string _filename;
+
         internal ConvertProcess(string filename, string newExtension, DownloadStatus downloadStatus) : base("ffmpeg", $"-i \"{filename}\" \"{Path.ChangeExtension(filename, newExtension)}\"", downloadStatus)
         {
+            _filename = filename;
+        }
+
+        private protected override void OnExited(bool killed)
+        {
+            if (!killed)
+            {
+                File.Delete(_filename);
+            }
         }
     }
 }

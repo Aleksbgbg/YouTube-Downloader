fdc7d13da9db16e81e7007ac103b280bc30d81bb
Aleksbgbg <kozichka01@abv.bg>
2018-05-30 14:09:09 +0100

    [ProcessTabs] Add conductor to show IProcessTabViewModels in a TabControl, based on the pre-existing CurrentDownloadsViewModel.

---
+++ YouTube Downloader/ViewModels/Process/ProcessTabsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace YouTube.Downloader.ViewModels.Process
+{
+    using Caliburn.Micro;
+
+    using YouTube.Downloader.ViewModels.Interfaces.Process;
+
+    internal class ProcessTabsViewModel : Conductor<IProcessTabViewModel>.Collection.OneActive, IProcessTabsViewModel
+    {
+        public ProcessTabsViewModel(
+                IDownloadsTabViewModel downloadsTabViewModel,
+                IConversionsTabViewModel conversionsTabViewModel,
+                ICompletedTabViewModel completedTabViewModel
+                )
+        {
+            Items.Add(downloadsTabViewModel);
+            Items.Add(conversionsTabViewModel);
+            Items.Add(completedTabViewModel);
+        }
+    }
+}

fa2296bf9dff6e63d0b13a8ee7886d9edc2751b2
Aleksbgbg <kozichka01@abv.bg>
2018-06-02 18:28:54 +0100

    [DigitalStorageManager] Add some casing variations.

--- YouTube Downloader/Core/DigitalStorageManager.cs
+++ YouTube Downloader/Core/DigitalStorageManager.cs
@@ -13,51 +13,60 @@
         {
             switch (units)
             {
+                case "b":
                 case "B":
                     return 1L;
 
+                case "kb":
                 case "Kb":
                     return 125L;
 
                 //case "Kib":
                 //    return 128L;
 
+                case "kB":
                 case "KB":
                     return 1000L;
 
                 case "KiB":
                     return 1024L;
 
+                case "mb":
                 case "Mb":
                     return 125L * 1000L;
 
                 //case "Mib":
                 //    return 131_072L;
 
+                case "mB":
                 case "MB":
                     return 1000L * 1000L;
 
                 case "MiB":
                     return 1024L * 1024L;
 
+                case "gb":
                 case "Gb":
                     return 125L * 1000L * 1000L;
 
                 //case "Gib":
                 //    return 134_200_000L;
 
+                case "gB":
                 case "GB":
                     return 1000L * 1000L;
 
                 case "GiB":
                     return 1024L * 1024L * 1024L;
 
+                case "tb":
                 case "Tb":
                     return 125L * 1000L * 1000L * 1000L;
 
                 //case "Tib":
                 //    return 137_400_000_000L;
 
+                case "tB":
                 case "TB":
                     return 1000L * 1000L * 1000L * 1000L;
 

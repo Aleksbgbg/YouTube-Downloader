6af7c85066d1b1a6b6ccf7eee1f94a0ab587afe6
Aleksbgbg <kozichka01@abv.bg>
2018-06-02 18:22:06 +0100

    [DownloadProcess] Use new methods from DigitalStorageManager.

--- YouTube Downloader/Core/Downloading/DownloadProcess.cs
+++ YouTube Downloader/Core/Downloading/DownloadProcess.cs
@@ -13,29 +13,6 @@
         {
                 new ParameterMonitoring("Progress", new Regex(@"^\[download] (?<ProgressPercentage>[ 1][ 0-9][0-9]\.[0-9])% of .*?(?<TotalDownloadSize>[\d\.]+)?(?<TotalDownloadSizeUnits>.iB) at +(?:(?<DownloadSpeed>.+)(?<DownloadSpeedUnits>.iB)\/s|Unknown speed)"), (currentValue, match) =>
                 {
-                    long GetBytes(double size, string units)
-                    {
-                        int GetMultiplier()
-                        {
-                            switch (units)
-                            {
-                                case "KiB":
-                                    return 1024;
-
-                                case "MiB":
-                                    return 1024 * 1024;
-
-                                case "GiB":
-                                    return 1024 * 1024 * 1024;
-
-                                default:
-                                    throw new InvalidOperationException("Invalid units for multiplier.");
-                            }
-                        }
-
-                        return (long)(size * GetMultiplier());
-                    }
-
                     long? GetDownloadSpeed()
                     {
                         string downloadSpeed = match.Groups["DownloadSpeed"].Value;
@@ -45,10 +22,10 @@
                             return null;
                         }
 
-                        return GetBytes(double.Parse(downloadSpeed), match.Groups["DownloadSpeedUnits"].Value);
+                        return DigitalStorageManager.GetBytes(double.Parse(downloadSpeed), match.Groups["DownloadSpeedUnits"].Value);
                     }
 
-                    long totalDownloadSize = GetBytes(double.Parse(match.Groups["TotalDownloadSize"].Value), match.Groups["TotalDownloadSizeUnits"].Value);
+                    long totalDownloadSize = DigitalStorageManager.GetBytes(double.Parse(match.Groups["TotalDownloadSize"].Value), match.Groups["TotalDownloadSizeUnits"].Value);
 
                     double progressPercentage = double.Parse(match.Groups["ProgressPercentage"].Value);
 

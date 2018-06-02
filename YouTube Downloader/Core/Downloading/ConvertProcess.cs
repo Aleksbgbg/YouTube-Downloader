6c3a4fe2fe49c70924e67a97af06e0321ea7ebb9
Aleksbgbg <kozichka01@abv.bg>
2018-06-02 13:18:13 +0100

    [ConvertProcess] Add progress monitoring.

--- YouTube Downloader/Core/Downloading/ConvertProcess.cs
+++ YouTube Downloader/Core/Downloading/ConvertProcess.cs
@@ -1,16 +1,60 @@
 ﻿namespace YouTube.Downloader.Core.Downloading
 {
+    using System;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Text.RegularExpressions;
 
+    using YouTube.Downloader.EventArgs;
     using YouTube.Downloader.Models.Download;
 
     internal class ConvertProcess : MonitoredProcess
     {
+        private static readonly ParameterMonitoring[] ParameterMonitorings =
+        {
+            new ParameterMonitoring("Progress", new Regex(@"(?<Key>\w+)= *(?<Value>.+?)(?: |$)"), (dict, match) =>
+            {
+                Dictionary<string, string> keyValuePairs = (Dictionary<string, string>)dict;
+
+                while (match != null)
+                {
+                    keyValuePairs[match.Groups["Key"].Value] = match.Groups["Value"].Value;
+
+                    match = match.NextMatch();
+                }
+
+                return keyValuePairs;
+            }, new Dictionary<string, string>())
+        };
+
         private readonly string _filename;
 
-        internal ConvertProcess(string filename, string newExtension, DownloadStatus downloadStatus) : base("ffmpeg", $"-i \"{filename}\" \"{Path.ChangeExtension(filename, newExtension)}\"", downloadStatus)
+        internal ConvertProcess(string filename, string newExtension, DownloadStatus downloadStatus)
+                :
+                base("ffmpeg",
+                     $"-i \"{filename}\" \"{Path.ChangeExtension(filename, newExtension)}\"",
+                     downloadStatus,
+                     ParameterMonitorings)
         {
             _filename = filename;
+        }
+
+        private protected override void OnStart()
+        {
+            ParameterMonitoring progressMonitoring = ProcessMonitor.ParameterMonitorings["Progress"];
+
+            ProcessMonitor.Finished += ProcessMonitorFinished;
+            progressMonitoring.ValueUpdated += ProgressUpdated;
+
+            void ProcessMonitorFinished(object sender, EventArgs e)
+            {
+                ProcessMonitor.Finished -= ProcessMonitorFinished;
+                progressMonitoring.ValueUpdated -= ProgressUpdated;
+            }
+
+            void ProgressUpdated(object sender, ParameterUpdatedEventArgs e)
+            {
+            }
         }
 
         private protected override void OnExited(bool killed)

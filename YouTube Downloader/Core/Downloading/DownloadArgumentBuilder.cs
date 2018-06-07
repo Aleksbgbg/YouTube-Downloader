3c46e6a37a398cc909a3ec1461e50bdf0793db79
Aleksbgbg <kozichka01@abv.bg>
2018-06-07 11:14:04 +0100

    [DownloadArgumentBuilder] Add builder class to format arguments for youtube-dl.

---
+++ YouTube Downloader/Core/Downloading/DownloadArgumentBuilder.cs
@@ -0,0 +1,56 @@
+﻿namespace YouTube.Downloader.Core.Downloading
+{
+    using System.Text;
+
+    using YouTube.Downloader.Models;
+    using YouTube.Downloader.Models.Download;
+
+    internal class DownloadArgumentBuilder
+    {
+        internal string DownloadFolder { get; set; }
+
+        internal DownloadType DownloadType { get; set; }
+
+        internal OutputFormat OutputFormat { get; set; }
+
+        internal string VideoName { get; set; }
+
+        internal string VideoId { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendFormat("-o \"{0}\\{1}.%(ext)s\"", DownloadFolder, VideoName);
+
+            stringBuilder.Append(" -f ");
+
+            if (DownloadType == DownloadType.AudioVideo)
+            {
+                if (OutputFormat == OutputFormat.Mp4)
+                {
+                    stringBuilder.AppendFormat("bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best");
+                }
+                else
+                {
+                    stringBuilder.Append("bestvideo+bestaudio/best");
+                }
+            }
+            else
+            {
+                stringBuilder.Append("bestaudio");
+
+                if (OutputFormat == OutputFormat.Mp3)
+                {
+                    stringBuilder.AppendFormat("[ext=mp3]");
+                }
+
+                stringBuilder.Append("/best");
+            }
+
+            stringBuilder.AppendFormat(" -- \"{0}\"", VideoId);
+
+            return stringBuilder.ToString();
+        }
+    }
+}

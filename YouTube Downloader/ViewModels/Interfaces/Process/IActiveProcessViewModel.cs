638d531502638bcefecb61381d3a89f16e00a1b6
Aleksbgbg <kozichka01@abv.bg>
2018-06-13 12:26:01 +0100

    [ActiveProcessViewModel] Add more specific ViewModel which implements a Process property so that ProcessViewModel doesn't have to.

---
+++ YouTube Downloader/ViewModels/Interfaces/Process/IActiveProcessViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace YouTube.Downloader.ViewModels.Interfaces.Process
+{
+    using YouTube.Downloader.Core.Downloading;
+
+    internal interface IActiveProcessViewModel : IProcessViewModel
+    {
+        MonitoredProcess Process { get; }
+    }
+}
---
+++ YouTube Downloader/ViewModels/Process/ActiveProcessViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace YouTube.Downloader.ViewModels.Process
+{
+    using YouTube.Downloader.Core.Downloading;
+    using YouTube.Downloader.ViewModels.Interfaces;
+    using YouTube.Downloader.ViewModels.Interfaces.Process;
+
+    internal abstract class ActiveProcessViewModel : ProcessViewModel, IActiveProcessViewModel
+    {
+        public MonitoredProcess Process { get; private set; }
+
+        private protected void Initialise(IVideoViewModel videoViewModel, MonitoredProcess process)
+        {
+            Initialise(videoViewModel);
+            Process = process;
+        }
+    }
+}

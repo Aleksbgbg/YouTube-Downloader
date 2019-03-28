e6a3b8d1986aa41ce0f66fdbbfa28c9613cb7305
Aleksbgbg <kozichka01@abv.bg>
2019-03-28 18:14:33 +0000

    [ShellViewModel] Update YoutubeDl before rendering.

--- YouTube Downloader/ViewModels/ShellViewModel.cs
+++ YouTube Downloader/ViewModels/ShellViewModel.cs
@@ -1,24 +1,29 @@
 ﻿namespace YouTube.Downloader.ViewModels
 {
+    using System.Threading.Tasks;
     using System.Windows;
 
     using Caliburn.Micro;
 
+    using YouTube.Downloader.Utilities;
     using YouTube.Downloader.ViewModels.Interfaces;
 
     internal sealed class ShellViewModel : Conductor<IViewModelBase>, IShellViewModel
     {
+        private readonly IMainViewModel _mainViewModel;
+
         private readonly ISettingsViewModel _settingsViewModel;
 
         public ShellViewModel(IMainViewModel mainViewModel, ISettingsViewModel settingsViewModel)
         {
             DisplayName = "YouTube Downloader";
 
+            _mainViewModel = mainViewModel;
             _settingsViewModel = settingsViewModel;
 
             settingsViewModel.Closed += (sender, e) => ChangeActiveItem(mainViewModel, false);
 
-            ActivateItem(mainViewModel);
+            Task.Run(Initialize);
         }
 
         public void Exit()
@@ -29,6 +34,13 @@
         public void OpenSettings()
         {
             ChangeActiveItem(_settingsViewModel, false);
+        }
+
+        private async Task Initialize()
+        {
+            await YouTubeDl.Update();
+
+            ActivateItem(_mainViewModel);
         }
     }
 }
